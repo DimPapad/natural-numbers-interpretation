957af3499bf51da29f89cdc1a052ad3b3a902bab
Dimitris Papado <papadogd@gmail.com>
2023-07-24 03:53:58 +0300

    - Client changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,70 @@
 package org.example;
 
 import org.example.clients.Client;
-import org.example.models.GreekPhoneNumberOutput;
+import org.example.exceptions.WrongInputException;
+import org.example.models.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        // Input from user
+        System.out.println("""
+                Input a sequence by numbers.
+                Each number must consist of max 3 digits.
+                Sequence must have at least one space character.
+                """);
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        System.out.println();
+
+        // Client actions
         Client client=new Client();
-        client.validateGreekPhoneNumberAndGetInterpretations();
+        // Input validation and get
+        Optional<Input> input=client.validateInputAndGet(userInput);
+        if (input.isEmpty()){
+            System.out.println("""
+                    The input sequence contains at least one number that has more than 3 digits.
+                    OR
+                    The sequence has not a space character
+                    TRY AGAIN!""");
+            System.exit(1);
+        }
+
+        // Get the number from sequence
+        SimpleNumber simpleNumber=client.getNumberFromSequence(input.get().getValue());
+        System.out.println("The Number");
+        System.out.println(simpleNumber.getNumber());
+        System.out.println();
+
+        // Number validation
+        System.out.println("Greek phone number Validation");
+        Optional<GreekPhoneNumber> optionalNumber=client.validateGreekPhoneNumberAndGet(input.get().getValue());
+        if (optionalNumber.isPresent()) {
+            System.out.println(optionalNumber.get().getNumber() + " [phone number: VALID]");
+        }
+        if (optionalNumber.isEmpty()){
+            System.out.println(simpleNumber.getNumber() + " [phone number: INVALID]");
+        }
+        System.out.println();
+
+        // Get number interpretations
+        System.out.println("Number Interpretations");
+        List<NumberEntity> interpretations=client.getNumberInterpretations(input.get().getValue());
+        for (NumberEntity interpretation : interpretations) {
+            if (interpretation instanceof GreekPhoneNumber) {
+                System.out.print(interpretation);
+                System.out.println(" [phone number: VALID]");
+            }
+            if (interpretation instanceof SimpleNumber) {
+                System.out.print(interpretation);
+                System.out.println(" [phone number: INVALID]");
+            }
+        }
     }
 
     
--- src/main/java/org/example/clients/Client.java
+++ src/main/java/org/example/clients/Client.java
@@ -5,70 +5,42 @@ import org.example.exceptions.WrongInputException;
 import org.example.models.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Client {
 
 
-    public void validateGreekPhoneNumberAndGetInterpretations() {
-        // Input
-        System.out.println("""
-                Input a sequence by numbers.
-                Each number must consist of max 3 digits.
-                Sequence must have at least one space character.
-                """);
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-
-        // Input validation
+    public Optional<Input> validateInputAndGet(String userInput) {
         Input input=new NumberInput();
         try {
             input.setValue(userInput);
+            return Optional.of(input);
         } catch (WrongInputException e) {
-            System.out.println("""
-                    The input sequence contains at least one number that has more than 3 digits.
-                    OR
-                    The sequence has not a space character
-                    TRY AGAIN!""");
-            System.exit(1);
+            return Optional.empty();
         }
-        String validInput=input.getValue();
+    }
 
-        // Print the number
-        System.out.println();
-        System.out.println("The Number");
+    public SimpleNumber getNumberFromSequence(String validInput) {
         SimpleNumber simpleNumber=new SimpleNumber();
         simpleNumber.setNumber(validInput);
-        System.out.println(simpleNumber.getNumber());
+        return simpleNumber;
+    }
 
-        // Number validation
-        System.out.println();
-        System.out.println("Greek phone number Validation");
+    public Optional<GreekPhoneNumber> validateGreekPhoneNumberAndGet(String validInput) {
         GreekPhoneNumber greekPhoneNumber = new GreekPhoneNumber();
         try {
             greekPhoneNumber.setNumber(validInput);
-            System.out.println(greekPhoneNumber.getNumber() + " [phone number: VALID]");
+            return Optional.of(greekPhoneNumber);
         } catch (WrongGreekPhoneNumberFormatException e) {
-            simpleNumber.setNumber(validInput);
-            System.out.println(simpleNumber.getNumber() + " [phone number: INVALID]");
+            return Optional.empty();
         }
+    }
 
-        // Print possible interpretations
-        System.out.println();
-        System.out.println("Possible Interpretations");
+    public List<NumberEntity> getNumberInterpretations(String validInput) {
         Output output = new GreekPhoneNumberOutput();
         output.setOutputs(validInput);
-        List<NumberEntity> outputs = output.getOutputs();
-        for (NumberEntity distinctOutput : outputs) {
-            if (distinctOutput instanceof GreekPhoneNumber) {
-                System.out.print(distinctOutput);
-                System.out.println(" [phone number: VALID]");
-            }
-            if (distinctOutput instanceof SimpleNumber) {
-                System.out.print(distinctOutput);
-                System.out.println(" [phone number: INVALID]");
-            }
-        }
+        return output.getOutputs();
     }
 
 
